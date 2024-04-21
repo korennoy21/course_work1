6033cc4079191fd74cc0a475eb8565f9dcce2921
korennoy21 <165386492+korennoy21@users.noreply.github.com>
2024-04-21 21:28:42 +0300

    get card-score,encryption

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 from src.utils import sort_status
 from src.utils import get_json
 from src.utils import sort_dates
+from src.utils import revers
 
 FILE_NAME = os.path.join('../data/operations.json')
 
@@ -10,7 +11,7 @@ def main():
     data_json = get_json(FILE_NAME)
     list_sorted_status = sort_status(data_json, state="EXECUTED")
     list_sorted_dates = sort_dates(list_sorted_status)
-
+    last_5_transaction = revers(list_sorted_dates[-5:])
 
 if __name__ == "__main__":
     main()
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import json
 import datetime
 
+
 def get_json(file_name):
     """Читает файл json"""
     with open(file_name, 'r', encoding="utf-8") as file:
@@ -30,3 +31,30 @@ def format_date(value):
     """Возвращает дату"""
     date = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
     return date.strftime('%d.%m.%Y')
+
+
+def get_from_to(dict_, from_, to_):
+    """Вернет откуда карта/счет, куда карта/счет"""
+    from_transaction = dict_.get(from_)
+    to_transaction = dict_.get(to_)
+    if from_transaction is None:
+        return False, to_transaction
+    return from_transaction, to_transaction
+
+
+def cipher(str_):
+    """на вход получает  <<Счет 65298957349197687907>>
+       или <<Visa Classic 3414396880443483>>
+       возвращает  номер счета в шифровaнном виде"""
+    if not str_:
+        return None
+    elif "счет" in str_.lower():
+        _name = str_.split()[:-1]
+        _number = str_.split()[-1]
+        result = f"{' '.join(_name)} **{_number[-4:]}"
+        return result
+    else:
+        card_name = str_.split()[:-1]
+        card_number = str_.split()[-1]
+        result = f"{' '.join(card_name)} {card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
+        return result
