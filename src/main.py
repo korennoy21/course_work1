94671a230bf46beaa6f926ea7b13be59c66ac784
korennoy21 <165386492+korennoy21@users.noreply.github.com>
2024-04-21 21:06:50 +0300

    sortirovka

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
+import os
+from src.utils import sort_status
 from src.utils import get_json
 
 
-def main():
-    pass
+FILE_NAME = os.path.join('../data/operations.json')
 
+
+def main():
+    data_json = get_json(FILE_NAME)
+
+
+    list_sorted_status = sort_status(data_json, state="EXECUTED")
 
 if __name__ == "__main__":
     main()
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,20 @@
 import json
-def get_json(file_name) -> list:
+
+
+def get_json(file_name):
     """Читает файл json"""
     with open(file_name, 'r', encoding="utf-8") as file:
         return json.load(file)
+
+
+def sort_status(json_list, state):
+    """Сортировка по статусу"""
+    result = []
+    for i in json_list:
+        if i.get('state') == state:
+            result.append(i)
+    return result
+
+def sort_dates(list_):
+    """cортирует по датам"""
+    return sorted(list_, key=lambda x: x['date'])
